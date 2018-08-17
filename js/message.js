ebd093890da8d24a09787787ce958b0b578ebb46
lanshuxia <1341193700@qq.com>
2018-08-17 17:27:41 +0800

    test

--- js/message.js
+++ js/message.js
@@ -12,9 +12,7 @@ var deviceId = 867223025154659;
 var checkedDevices=[]; //存放设备号
 
 $(function(){
-/*    $('button').click(function(){
-        videoCon()
-    })*/
+    videoCon()
     getLocation()
     extracted();
     getIndexData()
